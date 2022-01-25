e715157a0071af84d406ef49ea4ec3a4a6487590
GiaBaone <102190252@sv1.dut.udn.vn>
2022-01-25 23:19:11 +0700

    meta options base 25.1.22🛠

--- pythonapis/ecourses/courses/models.py
+++ pythonapis/ecourses/courses/models.py
@@ -10,11 +10,28 @@ class Category(models.Model): #course_category
     def __str__(self):
         return self.name
 
-class Course (models.Model):
-    subject = models.CharField(max_length=100,null=False)
-    description = models.TextField(null=True,blank=True)
+
+class ItemBase(models.Model):
+    class Meta:
+        abstract = True
+
+    subject = models.CharField(max_length=100, null=False)
+    image = models.ImageField(upload_to='courses/%Y/%m', default=None)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+class Course (ItemBase):
+    class Meta:
+        unique_together = ('subject', 'category')
+        ordering = ["-id"]
+
+    description = models.TextField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
+class Lesson(ItemBase):
+    class Meta:
+        unique_together = ('subject', 'course')
+
+    content = models.TextField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
