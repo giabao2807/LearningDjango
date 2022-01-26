8dab8159ef20ef101b46c4ccca113373dc2d7c3a
GiaBaone <102190252@sv1.dut.udn.vn>
2022-01-26 23:16:56 +0700

    custom admin page not show image 26.1.22

--- pythonapis/ecourses/courses/admin.py
+++ pythonapis/ecourses/courses/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
-from .models import Category,Lesson,Course,Tag
+from .models import Category, Lesson, Course, Tag
+from django.utils.html import mark_safe
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ["id", "subject", "created_date", "course", "active"]
+    search_fields = ["subject", "created_date", "course__subject"]
+    list_filter = ["subject", "course__subject"]
+    readonly_fields = ["avatar"]
+
+    def avatar(self, lesson):
+        return mark_safe("<img src='/static/{img_url}' alt='{alt}' width='120px'/>").format(img_url=lesson.image.name, alt=lesson.subject)
+
+
+class CourseAdmin(admin.ModelAdmin):
+    readonly_fields = ["avatar"]
+
+    def avatar(self, course):
+        return mark_safe('<img src="/static/{img_url}" alt="{alt}" width="120px" />').format(img_url=course.image.name, alt=course.subject)
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Lesson)
-admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Course, CourseAdmin)
+
+
