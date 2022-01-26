8a828fb7cd1420a50f0da6f7375e7e8df3bce521
GiaBaone <102190252@sv1.dut.udn.vn>
2022-01-26 21:44:00 +0700

    ManyToMany OneToMany OneToOne 26.1.22

--- pythonapis/ecourses/courses/models.py
+++ pythonapis/ecourses/courses/models.py
@@ -21,6 +21,9 @@ class ItemBase(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.subject
+
 class Course (ItemBase):
     class Meta:
         unique_together = ('subject', 'category')
@@ -29,9 +32,18 @@ class Course (ItemBase):
     description = models.TextField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Lesson(ItemBase):
     class Meta:
         unique_together = ('subject', 'course')
 
     content = models.TextField()
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, related_name="lessons", on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name="lessons", blank=True, null=True)
+
+
